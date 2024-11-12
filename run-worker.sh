755c09eb6a255457924a637370c4f59ad7391312
unicodeveloper <prosperotemuyiwa@gmail.com>
2024-11-12 16:25:48 +0100

    fix

--- run-worker.sh
+++ run-worker.sh
@@ -2,4 +2,4 @@
 # Make sure this file has executable permissions, run `chmod +x run-worker.sh`
 
 # This command runs the queue worker. 
-php bin/console messenger:consume async --time-limit=3600 --memory-limit=128M
+php bin/console messenger:consume async
